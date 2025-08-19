b943e5a45b76829068db4f9b023a633b313d2e61
jeter5274 <jeter5274@gmail.com>
2025-08-19 23:31:21 +0900

    사용자 포인트 충전/사용 내역 조회 테스트 구현

--- src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -1,8 +1,11 @@
 package io.hhplus.tdd.point;
 
+import io.hhplus.tdd.database.PointHistoryTable;
 import io.hhplus.tdd.database.UserPointTable;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.mockito.Mockito.*;
 
@@ -23,6 +26,21 @@ class PointServiceTest {
         UserPoint result = pointService.getUserPoint(userId);
 
         verify(mockUserPointTable, times(1)).selectById(userId);
+    }
+
+    @Test
+    @DisplayName("사용자 포인트 충전/사용 내역 조회 테스트")
+    void getUserPointHistoryList_mock() {
+
+        long userId = 1L;
+
+        PointHistoryTable mockPointHistoryTable = mock(PointHistoryTable.class);
+        PointService pointService = new PointService(mockPointHistoryTable);
+        when(mockPointHistoryTable.selectAllByUserId(userId)).thenReturn(List.of());
+
+        List<PointHistory> result = pointService.getUserPointHistoryList(userId);
+
+        verify(mockPointHistoryTable, times(1)).selectAllByUserId(userId);
     }
 
 }
