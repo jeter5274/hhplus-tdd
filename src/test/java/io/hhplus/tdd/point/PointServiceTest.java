1306f83341fb1ba4cac10960b6aff8716970e8ce
jeter5274 <jeter5274@gmail.com>
2025-08-19 23:34:21 +0900

    용자 포인트 사용 시 포인트 사용 테스트 구현

--- src/test/java/io/hhplus/tdd/point/PointServiceTest.java
+++ src/test/java/io/hhplus/tdd/point/PointServiceTest.java
@@ -83,4 +83,24 @@ class PointServiceTest {
         verify(mockPointHistoryTable, times(1)).insert(userId,amount, TransactionType.CHARGE, updateMillis);
     }
 
+    @Test
+    @DisplayName("사용자 포인트 사용 시 포인트 사용 테스트")
+    void setUserPoint_use_save_mock() {
+        long userId = 1L;
+        long point = 10;
+        long amount = 10;
+        long updateMillis = System.currentTimeMillis();
+
+        PointHistoryTable mockPointHistoryTable = mock(PointHistoryTable.class);
+        UserPointTable mockUserPointTable = mock(UserPointTable.class);
+        PointService pointService = new PointService(mockPointHistoryTable, mockUserPointTable);
+        when(mockUserPointTable.selectById(userId)).thenReturn(new UserPoint(userId, point, updateMillis));
+        when(mockUserPointTable.insertOrUpdate(userId, point-amount)).thenReturn(new UserPoint(userId, point-amount, updateMillis));
+        when(mockPointHistoryTable.insert(userId, amount, TransactionType.USE, updateMillis)).thenReturn(new PointHistory(userId, userId, amount, TransactionType.USE, updateMillis));
+
+        UserPoint result = pointService.setUserPoint(userId, amount, TransactionType.USE);
+
+        verify(mockUserPointTable, times(1)).insertOrUpdate(userId, point-amount);
+    }
+
 }
