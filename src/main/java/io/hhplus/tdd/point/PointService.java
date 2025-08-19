4d1fda6439cb4f0a781c2e7115b626c9141bface
jeter5274 <jeter5274@gmail.com>
2025-08-19 22:53:05 +0900

    사용자 포인트 충전/사용 내역 조회 TODO 구현

--- src/main/java/io/hhplus/tdd/point/PointService.java
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -9,6 +9,8 @@ public class PointService {
 
     UserPointTable userPointTable;
 
+    PointHistoryTable pointHistoryTable;
+
     /**
      * 사용자 포인트 조회 <br>
      * @param userId
@@ -17,6 +19,17 @@ public class PointService {
     UserPoint getUserPoint(long userId){
 
         return userPointTable.selectById(userId);
+
+    }
+
+    /**
+     * 사용자 포인트 충전/사용 내역 조회 <br>
+     * @param userId
+     * @return
+     */
+    List<PointHistory> getUserPointHistoryList(long userId){
+
+        return pointHistoryTable.selectAllByUserId(userId);
 
     }
 }
